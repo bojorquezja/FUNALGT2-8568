13bc86e72d5ecd63aaec0692f68a639d75eba148
Jeriko34 <goldplayer1207@gmail.com>
2024-10-17 08:42:22 -0500

    Primer salvado

---
+++ Personass/Entrenador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+   public class Entrenador
+    {
+        public string Nombre { get; set; }
+        public string Deporte { get; set; }
+        public void Haceejercicio()
+        {
+            Console.WriteLine("Nosotros hacemos mucho ejercicio!");
+        }
+        public void EntrenaEstudiantes()
+        {
+            Console.WriteLine("Los estudiantes entrenan muy duro para las olimpiadas!");
+        }
+
+    }
+}
---
+++ nousar/Personas/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    public class Estudiante
+    {
+     public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public void Correr()
+        {
+            Console.WriteLine("Yo corro muy rápido!");
+        }
+        public void Estudia()
+        {
+            Console.WriteLine("Yo estudio mucho!");
+        }
+    }
+}
