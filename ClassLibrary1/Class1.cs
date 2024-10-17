1bf17638dfeeb7e60f4d1d5f06931a61aaf47f63
JairGarcia29 <jamesjair29@gmail.com>
2024-10-17 08:21:05 -0500

    Segundo guardado

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class Estudiante
     {
+        public void Corre()
+        {   
+            Console.WriteLine("Correr es bueno para la salud");
+        }
+        public void Estudia() 
+        { 
+         Console.WriteLine("Estudiar alimenta nuestra mente");
+        }
+         
+    }
+    public class Entrenador 
+    {
+        public void HaceEjercicio()
+        {
+
+        }
+        public void EntrenaEstudiantes()
+        {
+
+        }
     }
 }
