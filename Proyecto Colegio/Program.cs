18b25e814f057869744249c7d342cbf28960f067
Diego-Ortiz-Ostolaza <diego.ortiz.joseph@gmail.com>
2024-10-17 08:29:52 -0500

    1er commit

---
+++ Proyecto Colegio/Program.cs
@@ -0,0 +1,9 @@
+﻿using Proyecto_Colegio;
+
+
+
+
+
+
+
+
---
+++ proyectopersonas/Entrenador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_personas
+{
+    public class Entrenador
+    {
+        public string Nombre { get; set; }
+        public string Deporte { get; set; }
+
+        public void Haceejericios () 
+        {
+            Console.WriteLine("Miren atentos el ejercicio..");
+        }
+
+        public void Entrenaestudiantes()
+        {
+            Console.WriteLine("Vamos a practicar atletismo.");
+        }
+                
+
+    }
+}
