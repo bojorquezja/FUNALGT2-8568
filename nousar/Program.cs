730457416b0c5d203da552b46497bff0f7245d96
JairGarcia29 <jamesjair29@gmail.com>
2024-10-17 08:55:12 -0500

    Quinto guardado

--- nousar/Program.cs
+++ nousar/Program.cs
@@ -1,4 +1,10 @@
-﻿using ClassLibrary1;
-using System.Diagnostics;
-Estudiante x = new Estudiante();
-Corre.x();
+﻿using Personas;
+
+Estudiante estudiante = new Estudiante("Juan", 20);
+Console.WriteLine(estudiante.ToString());
+estudiante.Corre();
+estudiante.Estudia();
+Entrenador entrenador = new Entrenador("Carlos", "Fútbol");
+Console.WriteLine(entrenador.ToString());
+entrenador.HaceEjercicio();
+entrenador.EntrenaEstudiantes();
