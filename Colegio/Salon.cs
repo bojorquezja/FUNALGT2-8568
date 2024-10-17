c81f416866d2e6667f9d44bee8b1bfabdb336ab2
ANDRE04PROGRAMER <andre.alarcon.2004@gmail.com>
2024-10-17 09:18:52 -0500

    Primer commit

---
+++ Colegio/Salon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Personas;
+
+namespace Colegio
+{
+    public class Salon
+    {
+        Entrenador Eu { get; set; }
+        Estudiante E1 { get; set; }
+        Estudiante E2 { get; set; }
+        Estudiante E3 { get; set; }
+        public void PasarAsistencia()
+        {
+            Console.WriteLine("HOLA " + Eu.Nombre);
+            Console.WriteLine("HOLA " + E1.Nombre);
+            Console.WriteLine("HOLA " + E2.Nombre);
+            Console.WriteLine("HOLA " + E3.Nombre);
+        }
+
+    }
+    
+
+}
---
+++ Personas/Estudiante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    public class Estudiante
+    {
+        public string Name { get; set; }
+        public int Edad { get; set; }
+        public void Corre()
+        {
+            Console.WriteLine("CORRE");
+        }
+        public void Estudia()
+        {
+            Console.WriteLine("ESTUDIA");
+        }
+        public void ToString()
+        {
+            Console.WriteLine("El nombre es " + Name);
+            Console.WriteLine("La edad es " + Edad);
+        }
+    }
+}
