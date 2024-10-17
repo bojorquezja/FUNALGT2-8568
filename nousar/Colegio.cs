a16efab979e98fc5da705a24a64083baedc41307
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-10-17 08:16:07 -0500

    examen

--- nousar/Colegio.cs
+++ nousar/Colegio.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using nousar;
+
+Estudiante es1 = new Estudiante();
+Estudiante es2 = new Estudiante();
+Estudiante es3 = new Estudiante();
+Entrenador en1 = new Entrenador();
+
+
+string Estud1, Estud2, Estud3, Entrenador;
+int edad;
+
+Console.Write("Ingrese el nombre del estidiante 1:");
+Estud1 = Console.ReadLine();
+Console.Write("\nAhora su edad: ");
+edad = int.Parse(Console.ReadLine());
+Console.WriteLine("\nIngrese el nombre del estidiante 2:");
+Estud2 = Console.ReadLine();
+Console.WriteLine("\nIngrese el nombre del estidiante 3:");
+Estud3 = Console.ReadLine();
+
+Console.WriteLine("\nIngrese el nobmre del entrenador:");
+Entrenador = Console.ReadLine();
+
+
+
