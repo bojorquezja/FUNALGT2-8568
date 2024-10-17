4b37af81cd6d554f48ed5c0031156b5cfce59fbe
Jeriko34 <goldplayer1207@gmail.com>
2024-10-17 08:57:01 -0500

    quinto com

--- Personass/Program.cs
+++ Personass/Program.cs
@@ -5,21 +5,23 @@ Salon salon = new Salon();
 Console.WriteLine("Ingrese el nombre del primer estudiante");
 Estudiante e1 = new Estudiante();
 e1.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la edad del primer estudiante");
 e1.Edad = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el nombre del segundo estudiante");
 Estudiante e2 = new Estudiante();
 e2.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la edad del segundo estudiante");
 e2.Edad = int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el nombre del tercer estudiante");
 Estudiante e3 = new Estudiante();
 e3.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese la edad del tercer estudiante");
 e3.Edad = int.Parse(Console.ReadLine());
 Console.WriteLine("Alumnos asignados al salon correspondiente");
 
 Console.WriteLine("Ingresa el nombre del entrenador");
 Entrenador en1 = new Entrenador();
 en1.Nombre = Console.ReadLine();
-Console.WriteLine("Ingrese la edad del entrenador");
 
 salon.Estu1 = e1;
 salon.Estu2 = e2;
