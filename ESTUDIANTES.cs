36f08cfb1397d0627112d2a0302c90f172e006c4
Akronixx <166467847+Akronixx@users.noreply.github.com>
2024-10-17 08:40:49 -0500

    avance

---
+++ ESTUDIANTES.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cole
+{
+    internal class ESTUDIANTES 
+    {
+        public string Direccion { get; set; }
+        public Delfin Delfin1 { get; set; }
+        public Delfin Delfin2 { get; set; }
+        public Orca Orca1 { get; set; }
+        public Orca Orca2 { get; set; }
+
+        public void ListaNombres()
+        {
+            Console.WriteLine($"Nombre: {Delfin1.Nombre}");
+            Console.WriteLine($"Edad: {Delfin2.Nombre}");
+            Console.WriteLine($"Nombre: {Orca1.Nombre}");
+            Console.WriteLine($"Deporte: {Orca2.Nombre}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using cole;
+
+Delfin d1 = new Delfin();
+d1.Nombre = "Luis";
+
+Delfin d2 = new Delfin();
+d2.Nombre = "12 años";
+
+Orca o1 = new Orca();
+o1.Nombre = "Marcelo";
+Orca o2 = new Orca();
+o2.Nombre = "Futbol";
+
+ESTUDIANTES a = new ESTUDIANTES();
+a.Direccion = "Av 1";
+a.Delfin1 = d1;
+a.Delfin2 = d2;
+a.Orca1 = o1;
+a.Orca2 = o2;
+Console.WriteLine(a.Direccion);
+a.ListaNombres();
